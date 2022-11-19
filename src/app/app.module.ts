683b1d720eec745a079fc4241d4f73eb8ac64179
Piotr Kita <piokit103@gmail.com>
2022-11-19 21:38:41 +0100

    added: Product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import {ProductListComponentModule} from "./components/product-list/product-list.component-module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        ProductListComponentModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
